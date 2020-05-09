2f9cbb8e3d1682122a4cd3e92b032d351af8e88d
GilbertHG <gilbertgoes11@gmail.com>
2020-05-09 16:55:47 +0800

    first commit

---
+++ imageSegmentation.m
@@ -0,0 +1,57 @@
+close all;  
+clear all;
+clc;
+img = imread('adji3.png');
+figure(1);
+imshow(img);
+
+imgGray = rgb2gray(img);
+imwrite(imgGray,'gray.jpg');
+figure(2);
+imshow(imgGray);
+title('GrayScale Image');
+
+figure(3);
+imhist(imgGray);
+title('Histogram')
+
+Len = length(imgGray);
+Hei = length(imgGray(:)) / length(imgGray);
+imgDiff = zeros(Len,Hei);
+
+for i=1: Hei-1;
+    for j = 1 : Len-1;
+        imgDiff(i,j) = abs(imgGray(i,j) - imgGray(i,j+1));
+        imgDiff(i,j) = imgDiff(i,j) + abs(imgGray(i,j+1) - imgGray(i,j));
+    end
+end
+for i=1: Hei-1;
+    for j = 1 : Len-1;
+        imgDiff(i,j) = imgDiff(i,j) + abs(imgGray(i,j) - imgGray(i+1,j));
+        imgDiff(i,j) = imgDiff(i,j) + abs(imgGray(i+1,j) - imgGray(i,j));
+    end
+end
+
+for i=2: Hei-1;
+    for j = 2 : Len-1;
+        imgDiff(i,j) = imgDiff(i,j) + abs(imgGray(i,j) - imgGray(i+1,j-1));
+        imgDiff(i,j) = imgDiff(i,j) + abs(imgGray(i-1,j+1) - imgGray(i,j));
+    end
+end
+
+
+figure(4);
+imshow(imgDiff);
+imwrite(imgDiff,'edge.jpg');
+title('Using my Algorithm');
+
+p = edge(imgGray, 'sobel' , 0.01, 'both');
+figure(5);
+imshow(p);
+title('Using MATLAB inbuilt Function');
+
+%figure(6);
+%subplot(2,2,1),imshow(img),title('Original Image');
+%subplot(2,2,2),imshow(imgGray),title('GrayScale Image');
+%subplot(2,2,3),imshow(imgDiff),title('Using my Algorithm');
+%subplot(2,2,4),imshow(p),title('Using Matlab inbuilt Function');
